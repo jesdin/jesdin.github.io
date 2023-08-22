aeb40461c88eff25252820d14593c957974c33df
Jesdin Raphael <jesdinraphael@gmail.com>
2023-08-22 07:42:40 -0400

    add skills

--- pages/_apps.js
+++ pages/_apps.js
@@ -1,11 +1,13 @@
 import "../styles/styles.css";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
+import Skills from "../components/Skills";
 
 export default function App({ Component, pageProps }) {
     return (
       <Layout>
         <Hero />
+        <Skills />
         {/* <Component {...pageProps} /> */}
       </Layout>
     );
